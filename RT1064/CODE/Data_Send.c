124534914aada0dcb93d573cf4a174290e1f7d19
MKJia <2495293223@qq.com>
2020-06-03 23:35:43 +0800

    0603_ALL_test_params

--- RT1064/CODE/Data_Send.c
+++ RT1064/CODE/Data_Send.c
@@ -90,7 +90,7 @@ void Send_Img(void)
       My_Put_Char(Mid[i]);
     }
     for(i=0;i<60;i++){
-      My_Put_Char(Rig[i]);//New_Rig[i]/10+40);
+      My_Put_Char(Rig[i]+1);//New_Rig[i]/10+40);
     }
 
     /*****************************************************/
@@ -204,16 +204,16 @@ void Variable_update(void)
 {
   Variable[0]= Cam_offset;//cntt;//ToF_distance;
   Variable[1]= Turn_Cam_Out;//cnt;//Turn_Cam_Out;//;//stat_slope;//gyroy_1;//Turn_Cam_Out;//Car_W;
-  Variable[2]= CarSpeed1;//speedTarget1; //Allwhiteend;
-  Variable[3]= CarSpeed2;//whitecnt;
+  Variable[2]= Lef_break_point;//Allwhiteend;//CarSpeed1;//speedTarget1; //Allwhiteend;
+  Variable[3]= Rig_break_point;//Allwhitestart;//CarSpeed2;//whitecnt;
   //  EM_Value_1=(float)(EM_Value_1*3.3/4096);
   // EM_Value_2=(float)(EM_Value_2*3.3/4096);
   // EM_Value_3=(float)(EM_Value_3*3.3/4096);
   // EM_Value_4=(float)(EM_Value_4*3.3/4096);
-  Variable[4]= EM_Value_1;//Cam_offset;
+  Variable[4]= Road2_flag;//EM_Value_1;//Cam_offset;
   Variable[5]= EM_Value_2;
   Variable[6]= EM_Value_3;
-  Variable[7]= EM_Value_4;
+  Variable[7]= Rig_slope;//EM_Value_4;
   Variable[8]= Lef_slope;//EM_offset;
   Variable[9]= Road;//speedTarget2;//Turn_Cam_Out;
   Variable[10]=Road0_flag;
