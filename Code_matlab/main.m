bfdb6ebe763afb40518ddf2fc32f261c73773cac
Dirreke <mingyang_ge@163.com>
2020-06-04 00:01:45 +0800

    0603-Update SWJ data

--- Code_matlab/main.m
+++ Code_matlab/main.m
@@ -1,27 +1,27 @@
-% clear all;clc;close all;
-% 
-% name='20200516_3';
-% % mykc20200513_10cm
+clear all;clc;close all;
+
+name='huan1';
+% mykc20200513_10cm
 Initial();
-% global New_Lef New_Rig New_Mid;
-% try load(strcat('./Data/',name,'.mat'))
-% catch
-%     pre_fun(name);
-%     load(strcat('./Data/',name,'.mat'))
-% end
-% No=13;
-% 
-% %% 取图
-% 
+global New_Lef New_Rig New_Mid;
+try load(strcat('./Data/',name,'.mat'))
+catch
+    pre_fun(name);
+    load(strcat('./Data/',name,'.mat'))
+end
+No=610;
+
+%% 取图
+
 % No=2;%429
-% Pic=Images{No};
-% Pic_L=Image_L(:,No);
-% % Pic_L=1:60';
-% Pic_M=Image_M(:,No);
-% Pic_R=Image_R(:,No);
-% figure
-% imshow(Pic)
-% clear Images Image_L Image_R Image_M No
+Pic=Images{No};
+Pic_L=Image_L(:,No);
+% Pic_L=1:60';
+Pic_M=Image_M(:,No);
+Pic_R=Image_R(:,No);
+figure
+imshow(Pic)
+clear Images Image_L Image_R Image_M No
 %% Matlab 自带函数边缘提取
 % Picedge=edge(Pic);
 
